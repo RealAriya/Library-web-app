2b121535e9e5930ac0711f3873559754e889c98d
Arya <arya.t.innovate@gmail.com>
2025-02-04 01:50:33 +0330

    feat
    + connect to MySQL database
    + fetch data from MySQL for display
    + display data on index page

--- script.py
+++ script.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+
+# Database setup
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:@localhost/Library"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
+# Create an object
+db = SQLAlchemy(app)
+
+
+class Members(db.Model):
+    ID = db.Column(db.Integer, primary_key = True)
+    FirstName = db.Column(db.String(20))
+    LastName = db.Column(db.String(20))
+    Email = db.Column(db.String(30))
+    Phone = db.Column(db.String(11))
+
+
+with app.app_context():
+    db.create_all()
+
+
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    result = Members.query.all()
+    return render_template("index.html", result=result)
 
 
 @app.route("/add", methods=["GET" , "POST"])
@@ -16,14 +41,17 @@ def add_memeber():
         email = request.form.get("Email")
         phone_number = request.form.get("Phone")
 
-        context = {
-            "first_name" : first_name,
-            "last_name" : last_name,
-            "email" : email,
-            "phone_number" : phone_number 
-        }
+        user_input = Members(
+            FirstName = first_name,
+            LastName = last_name,
+            Email = email,
+            Phone = phone_number
+         )
+        
+        db.session.add(user_input)
+        db.session.commit()
 
-        return render_template("index.html" , **context)
+        return redirect(url_for("index"))
    
     return render_template("form.html")
 
